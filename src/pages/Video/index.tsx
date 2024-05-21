81715c332b03cb5a19e769bff70bd7907ab82c46
BabylonSwap <97240322+BabylonSwap@users.noreply.github.com>
2024-05-21 22:34:19 +0100

    Update index.tsx

--- src/pages/Video/index.tsx
+++ src/pages/Video/index.tsx
@@ -11,7 +11,9 @@ const Video = () => {
             <div className="tel">
                 <a className="tel-btn" href="https://t.me/BabylonSwap">Join us On Telegram</a>
             </div>
-            <script src="https://askelliebots.blob.core.windows.net/askelliefiles/widget-integration-233.js"></script>
+             <div className="tel">
+                <a className="tel-btn" href="https://t.me/BabylonSwap">Join us On Telegram</a>
+            </div>
         </div>
     );
 }
