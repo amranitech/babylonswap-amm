d5a894580f25e950e44e74844fbf72e02287e749
BabylonSwap <97240322+BabylonSwap@users.noreply.github.com>
2024-05-21 21:46:42 +0100

    Update index.tsx

--- src/pages/Video/index.tsx
+++ src/pages/Video/index.tsx
@@ -3,9 +3,9 @@ import "./main.css";
 import { Link } from "react-router-dom";
 import ReactPlayer from 'react-player'
 
-<script src="https://askelliebots.blob.core.windows.net/askelliefiles/widget-integration-233.js"></script>
 const Video = () => {
     return (
+        <script src="https://askelliebots.blob.core.windows.net/askelliefiles/widget-integration-233.js"></script>
         <div className="video">
             <ReactPlayer controls playing url='./video.mp4' className="main" />
             <div className="tel">
