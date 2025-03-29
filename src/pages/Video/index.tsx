ae1e6aaffc55bb433c0c159aa52edab2e96e3dbc
Nabil Amrani <97240322+amranitech@users.noreply.github.com>
2025-03-29 16:43:42 +0000

    Update index.tsx

--- src/pages/Video/index.tsx
+++ src/pages/Video/index.tsx
@@ -7,6 +7,9 @@ import Script from 'next/script'
 const Video = () => {
     return (
         <div className="video">
+               <ReactPlayer controls playing url='./video.mp4' className="main" />
+            <div className="tel">
+                <a className="tel-btn" href="https://t.me/NabilAmrani">Join us On Telegram</a>
             </div>
         </div>
     );
