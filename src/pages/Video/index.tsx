3609589b51070648d0ae59f46ca6d57559aa7abd
Nabil Amrani <97240322+amranitech@users.noreply.github.com>
2025-03-29 16:22:51 +0000

    Update index.tsx

--- src/pages/Video/index.tsx
+++ src/pages/Video/index.tsx
@@ -7,9 +7,6 @@ import Script from 'next/script'
 const Video = () => {
     return (
         <div className="video">
-            <ReactPlayer controls playing url='./video.mp4' className="main" />
-            <div className="tel">
-                <a className="tel-btn" href="https://t.me/BabylonSwap">Join us On Telegram</a>
             </div>
         </div>
     );
